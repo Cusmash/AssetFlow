9046db8ed81af0a73fd0b6521ad72c97c0e7d290
Cusmash <iori_cusmash@hotmail.com>
2026-03-18 18:50:18 -0600

    adding storage class

---
+++ src/AssetFlow.Application/Abstractions/Storage/BlobUploadResult.cs
@@ -0,0 +1,11 @@
+﻿namespace AssetFlow.Application.Abstractions.Storage
+{
+    public sealed class BlobUploadResult
+    {
+        public string ContainerName { get; init; } = default!;
+        public string BlobName { get; init; } = default!;
+        public Uri BlobUri { get; init; } = default!;
+        public string ContentType { get; init; } = default!;
+        public long FileSizeBytes { get; init; }
+    }
+}
---
+++ src/AssetFlow.Application/Abstractions/Storage/IBlobStorageService.cs
@@ -0,0 +1,11 @@
+﻿namespace AssetFlow.Application.Abstractions.Storage
+{
+    public interface IBlobStorageService
+    {
+        Task<BlobUploadResult> UploadAsync(
+            Stream content,
+            string fileName,
+            string contentType,
+            CancellationToken cancellationToken = default);
+    }
+}
